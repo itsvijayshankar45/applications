f15f48751a688eac064a6d6afa67a8b08564077f
itsvijayshankar45 <129381018+itsvijayshankar45@users.noreply.github.com>
2023-03-30 15:07:27 +0530

    Cloud API Gateway

---
+++ esign-gateway-service/src/main/java/com/esign/EsignGatewayServiceApplication.java
@@ -0,0 +1,15 @@
+package com.esign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EsignGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsignGatewayServiceApplication.class, args);
+	}
+
+}
---
+++ esign-gateway-service/src/test/java/com/esign/EsignGatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsignGatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
